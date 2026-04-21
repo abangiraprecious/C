586e4a42fd84412f3690d83f44e1d5a8e127d078
Precious Abangira <abangiraprecious@gmail.com>
2026-04-21 23:22:23 +0300

    printf

--- recall/basic.c
+++ recall/basic.c
@@ -1,5 +1,6 @@
 #include <stdio.h> //importing a library for standard input/output
 
-int main () {//entry point of the program. Basically saying, start running my program from here
+int main () {//entry point of the program. Basically saying, start running my program from here. int = the type of value it returns (an integer)
+    printf("Hello, I am back to C");
 
 }
