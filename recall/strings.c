51a45a6ce9b6d20829e1ee5ddcbd658108a3e089
Precious Abangira <abangiraprecious@gmail.com>
2026-04-22 21:44:53 +0300

    strings

---
+++ recall/strings.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    char name[] = "Precious"; //char is used for caharters, letters. name[] - an array of characters
+    printf("My name is %s", name);
+
+    return 0;
+}
