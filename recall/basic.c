f0e7a18bab26d6fa0f3556db106a803b48f67cdc
Precious Abangira <abangiraprecious@gmail.com>
2026-04-22 21:31:55 +0300

    data types

--- recall/basic.c
+++ recall/basic.c
@@ -2,5 +2,5 @@
 
 int main () {//entry point of the program. Basically saying, start running my program from here. int = the type of value it returns (an integer)
     printf("Hello, I am back to C\n");
-    return 0;//return a value - excecuted succesfully. The program stops when it gets here. 
+    return 0;//return a value - excecuted succesfully. The program stops when it gets here. It sends a signal to the operating system.
 }
---
+++ recall/variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int age = 30;
+    printf("Hello, I am %d years old", age);
+
+    int score = 95;
+
+    printf("My score is %d", score);
+
+    return 0;
+
+    //%d is a placeholder the datat type integer
+}
