b30c66c6c0557cf0738abc04c6e55fd80d1960ce
Precious Abangira <abangiraprecious@gmail.com>
2026-04-22 21:54:40 +0300

    Grammar really

--- recall/strings.c
+++ recall/strings.c
@@ -3,8 +3,9 @@
 int main() {
     char name[] = "Anna"; //char is used for characters, letters. name[] - an array of characters
     printf("My name is %s\n", name); //%s - placeholder for strings
+    printf("\n");
 
-    printf("String and Integers in action\n");
+    printf("String and Integers in action below:\n");
 
     int age = 34;
     char myName[] = "Joy";
