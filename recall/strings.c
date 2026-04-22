8bce2648221b6f9bea4e50e678ad31e554fe1842
Precious Abangira <abangiraprecious@gmail.com>
2026-04-22 21:51:00 +0300

    correctedthe order of the variable names

--- recall/strings.c
+++ recall/strings.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    char name[] = "Precious"; //char is used for characters, letters. name[] - an array of characters
-    printf("My name is %s", name);
+    char name[] = "Anna"; //char is used for characters, letters. name[] - an array of characters
+    printf("My name is %s\n", name); //%s - placeholder for strings
+
+    printf("String and Integers\n");
+
+    int age = 34;
+    char myName[] = "Joy";
+
+    printf("My name is %s and I am %d years old\n", myName, age);
 
     return 0;
 }
