0261d482690b9668f22400f8700dba624d430a44
Precious Abangira <abangiraprecious@gmail.com>
2026-04-30 22:01:50 +0300

    not

---
+++ recall/or_logicoperators.c
@@ -0,0 +1,16 @@
+//OR || - only conditions needs be true
+#include <stdio.h>
+
+int main() {
+
+    int age;
+
+    printf("How old are you: ");
+    scanf("%d", &age);
+
+    if (age >= 18 || age < 60) {
+        printf("Working class\n");
+    }
+
+    return 0;
+}
