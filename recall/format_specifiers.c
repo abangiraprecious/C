375bb1a0a3c041e14c9dc713ba28582d17c16f7d
Precious Abangira <abangiraprecious@gmail.com>
2026-05-14 13:46:49 +0300

    width

--- recall/format_specifiers.c
+++ recall/format_specifiers.c
@@ -14,6 +14,18 @@ int main () {
     printf("The char is %c \n", punctuation);
     printf("The array/string is %s \n", array_name);
 
+    /*
+    Width
+    - specifies the minimum number of characters to print...typically before the value of the variable
+    */
+
+    int num1 = 1;
+    int num2 = 10;
+    int num3 = 100;
+    
+    printf("%3d\n", num1);
+    printf("%3d\n", num2);
+    printf("%4d\n", num3);
  
     return 0;
 }
